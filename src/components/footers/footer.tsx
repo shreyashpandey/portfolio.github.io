fb620118b47a3646fe0e24e8825edf59968d03d4
Shreyansh Pandey <shreyansh.p@lsq.net>
2021-08-28 22:49:41 +0530

    Initiated

---
+++ src/components/footers/footer.tsx
@@ -0,0 +1,19 @@
+import React,{ReactElement,FC} from "react";
+import { AppBar, Toolbar } from "@material-ui/core";
+interface Props{
+  title:string
+}
+const Footer:FC<any>=({title}):ReactElement=>{
+    return (
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <div>
+                      <p>This is a {title}</p>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+};
+export default Footer;  
